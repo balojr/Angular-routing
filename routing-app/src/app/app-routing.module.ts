a7f4736f3fa356f6476ba56f6e640fcc008edf91
balojr <77960542+balojr@users.noreply.github.com>
2023-05-12 11:57:15 +0300

    second commit

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,17 +1,54 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes, provideRouter } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { ResolveFn, RouterModule, RouterStateSnapshot, Routes, TitleStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
+import { Title } from '@angular/platform-browser';
 
+
+  @Injectable({providedIn: 'root'})
+  export class TemplatePageTitleStrategy extends TitleStrategy {
+    constructor(private readonly title: Title) {
+      super();
+    }
+
+    override updateTitle(routerState: RouterStateSnapshot) {
+      const title = this.buildTitle(routerState);
+      if (title !== undefined) {
+        this.title.setTitle(`Routing app | ${title}`);
+      }
+    }
+  }
+
+
+const resolvedChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
 
 export const routes: Routes = [ 
-  { path: 'first-component', component: FirstComponent },
+  { path: 'first-component', title: 'First component', component: FirstComponent,
+  children: [
+    {
+      path: 'child-a', /* child route path*/ title: resolvedChildATitle, component: ChildAComponent, // child route component that the router renders
+    },
+    {
+      path: 'child-b',/* child route path*/ title: 'Child B',component: ChildBComponent, // another child route component that the router renders
+    },
+  ],
+  },
+  
   { path: 'second-component', component: SecondComponent },
+  { path: '', redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },  
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
 import { provideRouter } from '@angular/router';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    PagenotfoundComponent,
+    ChildAComponent,
+    ChildBComponent
   ],
   imports: [
     BrowserModule,
